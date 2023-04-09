cde32ea9f89284aaa604908623521016a6d05a22
intoffset <intoffset@users.noreply.github.com>
2023-04-09 15:30:43 +0900

    Add openai service

---
+++ lib/service/openai_service.dart
@@ -0,0 +1,29 @@
+import 'package:dart_openai/openai.dart';
+
+class OpenAIService {
+  static const defaultChatModel = 'gpt-3.5-turbo';
+
+  static initialize(String apiKey) {
+    OpenAI.apiKey = apiKey;
+  }
+
+  Future<List<OpenAIModelModel>> getModels() async => await OpenAI.instance.model.list();
+
+  Future<OpenAIChatCompletionModel> postMessage(List<OpenAIChatCompletionChoiceMessageModel> messages,
+      {String model = defaultChatModel}) async {
+    final completion = await OpenAI.instance.chat.create(
+      model: model,
+      messages: messages,
+    );
+    return completion;
+  }
+
+  Stream<OpenAIStreamChatCompletionModel> postMessageStream(List<OpenAIChatCompletionChoiceMessageModel> messages,
+      {String model = defaultChatModel}) {
+    final stream = OpenAI.instance.chat.createStream(
+      model: model,
+      messages: messages,
+    );
+    return stream;
+  }
+}
